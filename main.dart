5dde51fb59dac813e7bc9d6c8606a61a52f4280b
akifiz <akif.asyrani@live.iium.edu.my>
2024-12-30 15:25:16 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,60 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CurrencyPage(),
-    );
-  }
-}
-
-class CurrencyPage extends StatefulWidget {
-  @override
-  _CurrencyPageState createState() => _CurrencyPageState();
-}
-
-class _CurrencyPageState extends State<CurrencyPage> {
-  double? conversionRate;
-
-  @override
-  void initState() {
-    super.initState();
-    fetchConversionRate();
-  }
-
-  Future<void> fetchConversionRate() async {
-    final apiKey = '1ee98bce2ddfa793c007ac3a'; // Your API key
-    final url = 'https://api.currencyconverterapi.com/api/v7/convert?q=MYR_USD&compact=ultra&apiKey=$apiKey';
-
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      setState(() {
-        conversionRate = data['MYR_USD'];
-      });
-    } else {
-      throw Exception('Failed to load exchange rate');
-    }
-  }
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Currency Converter'),
-      ),
-      body: Center(
-        child: conversionRate == null
-            ? CircularProgressIndicator()
-            : Text('1 MYR = \$${conversionRate!.toString()}'),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
