50733c0179103c2dcddea43228a3e472d0af5367
Khairil Hisham <khairil.h0703@gmail.com>
2025-01-21 22:19:31 +0800

    final balance and add expense button which leads to a dialogue window

--- abcmobileapp/lib/event_details.dart
+++ abcmobileapp/lib/event_details.dart
@@ -38,49 +38,55 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
         backgroundColor: AppColors.text,
         foregroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: true, // Messages start from the bottom
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                return ChatBubble(
-                  message: _messages[_messages.length - 1 - index],
-                  isSentByMe: true, // don't alternate sender
-                );
-              },
-            ),
+      body: Column(children: [
+        Expanded(
+          child: ListView.builder(
+            reverse: true, // Messages start from the bottom
+            itemCount: _messages.length,
+            itemBuilder: (context, index) {
+              return ChatBubble(
+                message: _messages[_messages.length - 1 - index],
+                isSentByMe: true, // don't alternate sender
+              );
+            },
           ),
-          Divider(height: 1),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        ),
+        Divider(height: 1),
+        Container(
+            color: AppColors.text,
+            padding: EdgeInsets.symmetric(vertical: 8),
             child: Center(
-        child: ElevatedButton(
-          child: 
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Dialog Title'),
-                  content: Text('This is the content of the dialog.'),
-                  actions: <Widget>[
-                    TextButton(
+                child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Calculate Balance")),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                      child: Text("Add an expense"),
                       onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('Close'),
-                    ),
-                  ],
-                );
-              },
-            );
-          ),
-            )
-          )
-        ]
-      ),
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Dialog Title'),
+                              content:
+                                  Text('This is the content of the dialog.'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Close'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                ])))
+      ]),
     );
   }
 }
@@ -102,7 +108,8 @@ class ChatBubble extends StatelessWidget {
         margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
         padding: EdgeInsets.all(12),
         decoration: BoxDecoration(
-          color: isSentByMe ? AppColors.sub : const Color.fromARGB(255, 0, 0, 0),
+          color:
+              isSentByMe ? AppColors.sub : const Color.fromARGB(255, 0, 0, 0),
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(12),
             topRight: Radius.circular(12),
