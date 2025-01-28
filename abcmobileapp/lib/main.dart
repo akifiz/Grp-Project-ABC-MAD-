dad2155b396b45a4049b8f0965d91b8444a9a55e
akifiz <akif.asyrani@live.iium.edu.my>
2025-01-28 22:03:33 +0800

    a

--- abcmobileapp/lib/main.dart
+++ abcmobileapp/lib/main.dart
@@ -9,6 +9,7 @@ import 'dart:convert';
 import 'settings.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
