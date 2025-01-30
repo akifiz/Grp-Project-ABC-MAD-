ee998b6c9c338534a38f09c376d5c3fcab39021c
Khairil Hisham <khairil.h0703@gmail.com>
2025-01-30 14:25:55 +0800

    currency button option list, currency feature done

--- abcmobileapp/lib/currency.dart
+++ abcmobileapp/lib/currency.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
   Future<double> getExchangeRate(String fromCurrency, String toCurrency) async {
     final String apiKey = '1ee98bce2ddfa793c007ac3a';
     final url = Uri.parse('https://v6.exchangerate-api.com/v6/$apiKey/latest/$fromCurrency');
-    
+
+    if(toCurrency==fromCurrency) return 1.0; 
     try {
       final response = await http.get(url);
 
